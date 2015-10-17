86a8467a36cd3f56699a0f9b8dc5d0bb69b443bd
Chandler Cord <chandlercord@gmail.com>
2015-10-17 11:02:42 +0000

    Added exit codes for check_disk, check_memory, and check_cpu.

--- check_system.sh
+++ check_system.sh
@@ -17,8 +17,9 @@ df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 " " $6 }' | 
   partition=$(echo $output | awk '{ print $3 }' )
 
   if [ ${usep} -ge ${DISK_CRIT} ]; then
+    echo "${log_date}: Disk space critical - ${partition} ($usep%)"
     echo "${log_date}: Disk space critical - ${partition} ($usep%)" | mail -s "$HOSTNAME: Disk space critical" -a "From: ${FROM_EMAIL}" ${EMAIL_ADDRESS}
-    exit
+    exit 1
   else
     echo "${log_date}: Disk space is OK - ${partition} ($usep%)"
   fi
@@ -34,7 +35,7 @@ PERCENTAGE=$(($MEM_USED*100/$MEM_TOTAL))
 
 if [ ${PERCENTAGE} -ge ${MEM_CRIT} ]; then
   echo "${log_date}: Memory usage critical - ${PERCENTAGE}" | mail -s "$HOSTNAME: Memory usage critical" -a "From: ${FROM_EMAIL}" ${EMAIL_ADDRESS}
-  exit
+  exit 2
 else
   echo "${log_date}: Memory usage OK - ${PERCENTAGE}"
 fi
@@ -45,12 +46,12 @@ check_cpu(){
   CPU_USAGE=`echo $[100-$(vmstat|tail -1|awk '{print $15}')]`
   if [ ${CPU_USAGE} -ge ${CPU_CRIT} ]; then
     echo "${log_date}: CPU usage critical - ${CPU_USAGE} percent" | mail -s "$HOSTNAME: CPU usage critical" -a "From: ${FROM_EMAIL}" ${EMAIL_ADDRESS}
-    exit
+    exit 3
   else
     echo "${log_date}: CPU usage OK - ${CPU_USAGE} percent"
   fi
 }
 
-check_disk
-check_mem
-check_cpu
+check_disk || if [ $? == "1" ]; then echo "Disk critical"; exit 1; fi
+check_mem || if [ $? == "2" ]; then echo "Memory critical"; exit 2; fi
+check_cpu || if [ $? == "3" ]; then echo "CPU critical"; exit 3; fi
